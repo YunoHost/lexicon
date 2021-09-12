ae6e6a44e9e6bf1ab16c098d806d737c4fcee78f
Alexandre Aubin <alex.aubin@mailoo.org>
2021-09-12 14:02:19 +0200

    Various tweaks to make the build work

--- setup.py
+++ setup.py
@@ -1,6 +1,82 @@
-#!/usr/bin/env python
+"""A setuptools based setup module.
 
-import setuptools
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
 
-if __name__ == "__main__":
-    setuptools.setup()
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+# Always prefer setuptools over distutils
+from setuptools import find_packages, setup
+import subprocess
+
+version = subprocess.check_output(['head', 'debian/changelog', '-n1']).decode().split()[1].strip('()').split('~')[0].split('-')[0].split('+')[0]
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, "README.rst"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="dns-lexicon",
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version=version,
+    description="Manipulate DNS records on various DNS providers in a standardized/agnostic way",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    # The project's main homepage.
+    url="https://github.com/AnalogJ/lexicon",
+    # Author details
+    author="Jason Kulatunga",
+    author_email="jason@thesparktree.com",
+    license="MIT",
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Internet :: Name Service (DNS)",
+        "Topic :: System :: Systems Administration",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=find_packages(exclude=["contrib", "docs", "tests"]),
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=[
+        # Core dependencies
+        "tldextract",
+        "future",
+        "cryptography",
+        "pyyaml",
+        # REST APIs
+        "requests[security]",
+        # SOAP APIs
+        "zeep",
+        # HTML scraping
+        "beautifulsoup4",
+    ],
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={"console_scripts": ["lexicon=lexicon.cli:main",],},
+)
